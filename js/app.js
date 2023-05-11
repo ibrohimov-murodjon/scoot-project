ddc88072dc22f1ef67ce9a10cf053f3c655c5054
ibrohimov-murodjon <murodjon.ibrohimov.rishton@gmail.com>
2023-05-11 11:07:52 +0500

    scoot project first commit

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
-const headerToggleBtn = document.querySelector('.hamburg__open-btn')
-const headerInnerWrapper = document.querySelector('.header__content')
+const headerToggleBtn = document.querySelector(".hamburg__open-btn");
+const headerInnerWrapper = document.querySelector(".header__content");
 
+const hamburgImg = document.getElementById("hamburgopen");
 
-  const  hamburgImg = document.getElementById("hamburgopen")
+headerToggleBtn.addEventListener("click", () => {
+  headerInnerWrapper.classList.add("show-nav");
+});
+
 let toggle = true;
-
-headerToggleBtn.addEventListener('click', () => {
-  headerInnerWrapper.classList.add('show-nav');
-})
-
-hamburgImg.addEventListener('click', () => {
+hamburgImg.addEventListener("click", () => {
   toggle = !toggle;
-  if(toggle){
-    hamburgImg.src = "image/hamburgopen.svg";
-    headerToggleBtn.addEventListener('click', () => {
-      headerInnerWrapper.classList.remove('show-nav');
-    })
-  } else{
-    hamburgImg.src="image/hamburgclose.svg"
-    headerToggleBtn.addEventListener('click', () => {
-      headerInnerWrapper.classList.add('show-nav');
-    })
+
+  if (toggle) {
+    hamburgImg.src = "image/open.svg";
+    hamburgImg.addEventListener("click", () => {
+      alert("Image is not found");
+    });
+  } else {
+    hamburgImg.src = "image/hamburgclose.svg";
+    headerToggleBtn.addEventListener("click", () => {
+      headerInnerWrapper.classList.add("show-nav");
+    });
   }
-})
+});
